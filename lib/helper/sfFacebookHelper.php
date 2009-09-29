a0a79f303d542291488393157451d2897d6ca8bc
Fabrice Bernhard <fabriceb@theodo.fr>
2009-09-29 18:27:41 +0000

    [sfFacebookConnectPlugin] Use the fb_url_for function to easily reference urls in a canvas application

--- lib/helper/sfFacebookHelper.php
+++ lib/helper/sfFacebookHelper.php
@@ -14,13 +14,15 @@
  * @author fabriceb
  * @since 2009-06-03
  */
-function fb_url_for($internal_uri)
+function fb_url_for()
 {
+  $arguments = func_get_args();
+    
   $host = '';
   if (sfFacebook::inCanvas())
   {
     $host = sfConfig::get('app_facebook_app_url'); 
   }
 
-  return $host.url_for($internal_uri, false);
+  return $host.call_user_func_array('url_for', $arguments);
 }
