58e918b8457778eb1a3835b469ac10044b0921f1
Fabrice Bernhard <fabriceb@theodo.fr>
2009-10-07 08:40:44 +0000

    [sfFacebookConnectPlugin] Because of js rendering problems with IE on heavy ajax websites, it is necessary to make sur any facebook js loads at the end of the page and make sur the Facebook connect button waits until then.

--- modules/sfFacebookConnectDemo/templates/indexSuccess.php
+++ modules/sfFacebookConnectDemo/templates/indexSuccess.php
@@ -1,20 +1,31 @@
 <?php use_helper('sfFacebookConnect'); ?>
-
+<?php 
+  slot('fb_connect');
+  include_facebook_connect_script();
+  end_slot();
+?>
 
 <h1>
-Welcome <fb:name uid="<?php echo $sf_user->getCurrentFacebookUid() ?>" useyou="false" ></fb:name> !
-<fb:profile-pic uid="<?php echo $sf_user->getCurrentFacebookUid() ?>" linked="true" ></fb:profile-pic>
+  Welcome <fb:name uid="<?php echo $sf_user->getCurrentFacebookUid() ?>" useyou="false" ></fb:name> !
+  <fb:profile-pic uid="<?php echo $sf_user->getCurrentFacebookUid() ?>" linked="true" ></fb:profile-pic>
 </h1>
-<br />
-<br />
-<div>
-<?php if ($sf_user->isAuthenticated()): ?>
-  Tu es connecté et ton login est : <?php echo $sf_user->getGuardUser()->getUsername() ?>
-<?php else: ?>
-  Tu n'es pas connecté... Connecte toi avec Facebook connect !
-  <?php echo facebook_connect_button(); ?>
-<?php endif; ?>
 
-<?php include_bottom_facebook_connect_script(); ?>
+<br />
+<br />
+
+<div>
+  <?php if ($sf_user->isAuthenticated()): ?>
+    Tu es connecté et ton login est : <?php echo $sf_user->getGuardUser()->getUsername() ?>
+  <?php else: ?>
+    Tu n'es pas connecté... Connecte toi avec Facebook connect !
+    <?php echo facebook_connect_button(); ?>
+  <?php endif; ?>
 </div>
 
+
+
+
+<!-- just before body in layout to avoid problems in IE -->
+<?php if (has_slot('fb_connect')): ?>
+  <?php include_slot('fb_connect') ?>
+<?php endif; ?>
