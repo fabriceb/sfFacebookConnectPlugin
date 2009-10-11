576511c9f6bce42fbe788813c853679a8204fe08
Fabrice Bernhard <fabriceb@theodo.fr>
2009-10-11 20:17:35 +0000

    [trunk] bug de js sur fb

--- lib/helper/sfFacebookHelper.php
+++ lib/helper/sfFacebookHelper.php
@@ -26,3 +26,29 @@ function fb_url_for()
 
   return $host.call_user_func_array('url_for', $arguments);
 }
+
+/**
+ * 
+ * @param string $src
+ * @param string $html_options
+ * @param string $fb_options
+ * @return void
+ * @author fabriceb
+ * @since Oct 11, 2009
+ */
+function fb_iframe($src, $html_options = '', $fb_options = '')
+{
+  if (!sfFacebook::inCanvas())
+  {
+    ?>
+    <iframe src="<?php echo $src ?>" <?php echo $html_options ?>></iframe>
+    <?php
+  }
+  else
+  {
+     ?>
+    <fb:iframe src="<?php echo $src ?>" <?php echo $html_options ?> <?php echo $fb_options ?>></fb:iframe>
+    <?php
+  }
+    
+}
