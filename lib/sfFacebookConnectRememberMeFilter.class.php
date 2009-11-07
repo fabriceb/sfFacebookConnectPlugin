20f54e71d2f808b64e147d78964536f076b1c0d9
Fabrice Bernhard <fabriceb@theodo.fr>
2009-11-07 18:47:38 +0000

    [sfFacebookConnectPlugin] basic remember me filter

---
+++ lib/sfFacebookConnectRememberMeFilter.class.php
@@ -0,0 +1,38 @@
+<?php
+
+/** 
+ * 
+ * Based on sfGuardRememberMeFilter
+ *
+ *  Place this filter before the security filter in filters.yml e.g.
+ *  ...
+ *  facebook_connect_remember_me:
+ *    class: sfFacebookConnectRememberMeFilter
+ *  security: ~
+ *  ...
+ *
+ * @package    sfFacebookConnectPlugin
+ * @author     fabriceb
+ */
+class sfFacebookConnectrememberMeFilter extends sfFilter
+{
+
+  /**
+   * @see sfFilter
+   */
+  public function execute($filterChain)
+  {
+
+    if ($this->isFirstCall() && $this->context->getUser()->isAnonymous())
+    {
+      $sfGuardUser = sfFacebook::getSfGuardUserByFacebookSession();
+
+      if ($sfGuardUser)
+      {
+        $this->getContext()->getUser()->signIn($sfGuardUser, true);
+      }
+    }
+
+    $filterChain->execute();
+  }
+}
